e7f90bcf8c65509ad4854a61595003a222c84fd3
Justin - PC <justinjoyce88@gmail.com>
2018-05-24 12:46:54 -0400

    Initial commit

---
+++ src/components/Skills.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="hello">
+    <div class="holder">
+      <form @submit.prevent="addSkill">
+        <input type="text" placeholder="Enter a skill you possess" v-model="skill" v-validate="'min:5'" name="skill">
+        <transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
+          <p class="alert" v-if="errors.has('skill')">{{errors.first('skill')}}</p>
+        </transition>
+      </form>
+      <ul>
+        <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
+          <li v-for="(data, index) in skills" :key="index">{{data.skill}}
+            <i class="fa fa-minus-circle" v-on:click="remove(index)"></i>
+          </li>
+
+        </transition-group>
+      </ul>
+      <p>Your Skills.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Skills",
+  data() {
+    return {
+      skill: "",
+      skills: [{ skill: "Vue.js" }, { skill: "Front End Developer" }],
+      alertObject: {
+        alert: true
+      }
+    };
+  },
+  methods: {
+    addSkill() {
+      this.$validator.validateAll().then(result => {
+        if (result) {
+          this.skills.push({ skill: this.skill });
+          this.skill = "";
+        }
+      });
+    },
+    remove(id) {
+      this.skills.splice(id, 1);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
+@import url("https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.css");
+.holder {
+  background: #fff;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+ul li {
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #e0edf4;
+  border-left: 5px solid #3eb3f6;
+  margin-bottom: 2px;
+  color: #3e5252;
+}
+
+p {
+  text-align: center;
+  padding: 30px 0;
+  color: gray;
+}
+
+.container {
+  box-shadow: 0px 0px 40px lightgray;
+}
+
+input {
+  width: calc(100% - 40px);
+  border: 0;
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #323333;
+  color: #687f7f;
+}
+
+.alert {
+  background: #fdf2ce;
+  font-weight: bold;
+  display: inline-block;
+  padding: 5px;
+  margin-top: -20px;
+}
+
+i {
+  float: right;
+  cursor: pointer;
+}
+</style>
